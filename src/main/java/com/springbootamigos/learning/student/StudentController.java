f37bee62f4f2cc0788af093b982797f44c4b194b
Amit Kumar <amit.bit.it@gmail.com>
2022-12-08 20:45:53 +0530

    Update & Delete method added

--- src/main/java/com/springbootamigos/learning/student/StudentController.java
+++ src/main/java/com/springbootamigos/learning/student/StudentController.java
@@ -3,6 +3,7 @@ package com.springbootamigos.learning.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -21,5 +22,19 @@ public class StudentController {
     @PostMapping
     public void registerNewStudent(@RequestBody Student student) throws Exception {
         studentService.addNew(student);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteStudentById(@PathVariable Long id) {
+        studentService.deleteStudentById(id);
+    }
+
+    @PutMapping("{id}")
+    public void updateStudentById(@PathVariable Long id,
+                                  @RequestParam(required = false) String name,
+                                  @RequestParam(required = false) String email,
+                                  @RequestParam(required = false) String dob) throws Exception {
+
+        studentService.updateStudent(id, name, email, dob);
     }
 }
--- src/main/java/com/springbootamigos/learning/student/StudentService.java
+++ src/main/java/com/springbootamigos/learning/student/StudentService.java
@@ -3,6 +3,9 @@ package com.springbootamigos.learning.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.text.ParseException;
+import java.time.LocalDate;
 import java.util.List;
 @Service
 public class StudentService {
@@ -28,5 +31,38 @@ public class StudentService {
         else {
             throw new IllegalArgumentException("Email already registered");
         }
+    }
+
+    public void deleteStudentById(Long id) {
+
+        if(studentRepo.existsById(id))
+        {
+            studentRepo.deleteById(id);
+        }
+        else throw new IllegalArgumentException("Student with id " + id + " does not exist");
+    }
+
+
+    @Transactional
+    public void updateStudent(Long id, String name, String email, String dob) throws Exception {
+
+        if(studentRepo.existsById(id))
+        {
+            Student student = studentRepo.findById(id).get();
+            if(!name.isEmpty()) student.setName(name);
+            if(!email.isEmpty()) student.setEmail(email);
+            if(!dob.isEmpty())
+            {
+                try {
+                    LocalDate ld = LocalDate.parse(dob);
+                    student.setDob(ld);
+                }
+                catch (Exception ex)
+                {
+                    throw new ParseException("DOB should be in yyyy-mm-dd format.", 0);
+                }
+            }
+        }
+        else throw new IllegalArgumentException("Student with id '" + id + "' does not exist.");
     }
 }
